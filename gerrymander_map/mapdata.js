24cca1accffc363252ffa840498255755c8f6408
ddhorstman <davidzilla27@gmail.com>
2019-12-19 20:11:23 -0500

    Add gerrymandering map

---
+++ gerrymander_map/mapdata.js
@@ -0,0 +1,723 @@
+var simplemaps_usmap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "State Description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Add location markers using latitude and longitude!",
+    location_color: "#2041D4",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "00000",
+    label_hover_color: "000000",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "/static/lib/simplemaps/map_images/",
+    fade_time: 0.1,
+    import_labels: "no",
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    HI: {
+      name: "Hawaii",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Democrats have a slight edge"
+    },
+    AK: {
+      name: "Alaska",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    FL: {
+      name: "Florida",
+      color: "#FF8282",
+      hover_color: "#FF8282",
+      description: "4,241,902 votes 'stolen'"
+    },
+    NH: {
+      name: "New Hampshire",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Democrats have a slight edge"
+    },
+    VT: {
+      name: "Vermont",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    ME: {
+      name: "Maine",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    RI: {
+      name: "Rhode Island",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Democrats have a slight edge"
+    },
+    NY: {
+      name: "New York",
+      color: "#BCBCFF",
+      hover_color: "#BCBCFF",
+      description: "1,659,099 votes 'stolen'"
+    },
+    PA: {
+      name: "Pennsylvania",
+      color: "#FF1010",
+      hover_color: "#FF1010",
+      description: "10,755,288 votes 'stolen'"
+    },
+    NJ: {
+      name: "New Jersey",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    DE: {
+      name: "Delaware",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    MD: {
+      name: "Maryland",
+      color: "#A1A1FF",
+      hover_color: "#A1A1FF",
+      description: "2,865,726 votes 'stolen'"
+    },
+    VA: {
+      name: "Virginia",
+      color: "#FFAAAA",
+      hover_color: "#FFAAAA",
+      description: "2,464,395 votes 'stolen'"
+    },
+    WV: {
+      name: "West Virginia",
+      color: "#FFA2A2",
+      hover_color: "#FFA2A2",
+      description: "2,797,794 votes 'stolen'"
+    },
+    OH: {
+      name: "Ohio",
+      color: "#FF3030",
+      hover_color: "#FF3030",
+      description: "7,861,585 votes 'stolen'"
+    },
+    IN: {
+      name: "Indiana",
+      color: "#FFB7B7",
+      hover_color: "#FFB7B7",
+      description: "1,872,074 votes 'stolen'"
+    },
+    IL: {
+      name: "Illinois",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    CT: {
+      name: "Connecticut",
+      color: "#9797FF",
+      hover_color: "#9797FF",
+      description: "3,310,799 votes 'stolen'"
+    },
+    WI: {
+      name: "Wisconsin",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Republicans have a slight edge"
+    },
+    NC: {
+      name: "North Carolina",
+      color: "#FF2424",
+      hover_color: "#FF2424",
+      description: "8,415,339 votes 'stolen'"
+    },
+    DC: {
+      name: "District of Columbia",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    MA: {
+      name: "Massachusetts",
+      color: "#3131FF",
+      hover_color: "#3131FF",
+      description: "7,839,585 votes 'stolen'"
+    },
+    TN: {
+      name: "Tennessee",
+      color: "#FFC5C5",
+      hover_color: "#FFC5C5",
+      description: "1,228,170 votes 'stolen'"
+    },
+    AR: {
+      name: "Arkansas",
+      color: "#FFBEBE",
+      hover_color: "#FFBEBE",
+      description: "1,552,064 votes 'stolen'"
+    },
+    MO: {
+      name: "Missouri",
+      color: "#FFC8C8",
+      hover_color: "#FFC8C8",
+      description: "1,096,586 votes 'stolen'"
+    },
+    GA: {
+      name: "Georgia",
+      color: "#FF7070",
+      hover_color: "#FF7070",
+      description: "5,033,540 votes 'stolen'"
+    },
+    SC: {
+      name: "South Carolina",
+      color: "#FFA1A1",
+      hover_color: "#FFA1A1",
+      description: "2,859,858 votes 'stolen'"
+    },
+    KY: {
+      name: "Kentucky",
+      color: "#FFCECE",
+      hover_color: "#FFCECE",
+      description: "838,243 votes 'stolen'"
+    },
+    AL: {
+      name: "Alabama",
+      color: "#FFBEBE",
+      hover_color: "#FFBEBE",
+      description: "1,537,976 votes 'stolen'"
+    },
+    LA: {
+      name: "Louisiana",
+      color: "#FFBFBF",
+      hover_color: "#FFBFBF",
+      description: "1,495,337 votes 'stolen'"
+    },
+    MS: {
+      name: "Mississippi",
+      color: "#FFC1C1",
+      hover_color: "#FFC1C1",
+      description: "1,421,046 votes 'stolen'"
+    },
+    IA: {
+      name: "Iowa",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Republicans have a slight edge"
+    },
+    MN: {
+      name: "Minnesota",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Democrats have a slight edge"
+    },
+    OK: {
+      name: "Oklahoma",
+      color: "#FFB7B7",
+      hover_color: "#FFB7B7",
+      description: "1,855,664 votes 'stolen'"
+    },
+    TX: {
+      name: "Texas",
+      color: "#FF1010",
+      hover_color: "#FF1010",
+      description: "22,184,633 votes 'stolen'"
+    },
+    NM: {
+      name: "New Mexico",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    KS: {
+      name: "Kansas",
+      color: "#FFBABA",
+      hover_color: "#FFBABA",
+      description: "1,749,448 votes 'stolen'"
+    },
+    NE: {
+      name: "Nebraska",
+      color: "#FFD0D0",
+      hover_color: "#FFD0D0",
+      description: "752,399 votes 'stolen'"
+    },
+    SD: {
+      name: "South Dakota",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    ND: {
+      name: "North Dakota",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    WY: {
+      name: "Wyoming",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    MT: {
+      name: "Montana",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    CO: {
+      name: "Colorado",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    UT: {
+      name: "Utah",
+      color: "#FFB9B9",
+      hover_color: "#FFB9B9",
+      description: "1,799,093 votes 'stolen'"
+    },
+    AZ: {
+      name: "Arizona",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    NV: {
+      name: "Nevada",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Democrats have a slight edge"
+    },
+    OR: {
+      name: "Oregon",
+      color: "#C5C5FF",
+      hover_color: "#C5C5FF",
+      description: "1,236,146 votes 'stolen'"
+    },
+    WA: {
+      name: "Washington",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Represented fairly"
+    },
+    CA: {
+      name: "California",
+      color: "#1010FF",
+      hover_color: "#1010FF",
+      description: "14,549,880 votes 'stolen'"
+    },
+    MI: {
+      name: "Michigan",
+      color: "#FFA2A2",
+      hover_color: "#FFA2A2",
+      description: "2,795,106 votes 'stolen'"
+    },
+    ID: {
+      name: "Idaho",
+      color: "#F5F5F5",
+      hover_color: "#CCCCCC",
+      description: "Republicans have a slight edge"
+    },
+    GU: {
+      name: "Guam",
+      hide: "yes"
+    },
+    VI: {
+      name: "Virgin Islands",
+      hide: "yes"
+    },
+    PR: {
+      name: "Puerto Rico",
+      hide: "yes"
+    },
+    MP: {
+      name: "Northern Mariana Islands",
+      hide: "yes"
+    },
+    AS: {
+      name: "American Samoa",
+      hide: "yes"
+    }
+  },
+  locations: {
+    "0": {
+      name: "New York City",
+      lat: 40.7143528,
+      lng: -74.0059731
+    },
+    "1": {
+      name: "Anchorage",
+      lat: 61.2180556,
+      lng: -149.9002778
+    }
+  },
+  labels: {
+    NH: {
+      parent_id: "NH",
+      x: "932",
+      y: "183",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    VT: {
+      parent_id: "VT",
+      x: "883",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    RI: {
+      parent_id: "RI",
+      x: "932",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    NJ: {
+      parent_id: "NJ",
+      x: "883",
+      y: "273",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DE: {
+      parent_id: "DE",
+      x: "883",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MD: {
+      parent_id: "MD",
+      x: "932",
+      y: "303",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    DC: {
+      parent_id: "DC",
+      x: "884",
+      y: "332",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    MA: {
+      parent_id: "MA",
+      x: "932",
+      y: "213",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    CT: {
+      parent_id: "CT",
+      x: "932",
+      y: "243",
+      pill: "yes",
+      width: 45,
+      display: "all"
+    },
+    HI: {
+      parent_id: "HI",
+      x: 305,
+      y: 565,
+      pill: "yes"
+    },
+    AK: {
+      parent_id: "AK",
+      x: "113",
+      y: "495"
+    },
+    FL: {
+      parent_id: "FL",
+      x: "773",
+      y: "510"
+    },
+    ME: {
+      parent_id: "ME",
+      x: "893",
+      y: "85"
+    },
+    NY: {
+      parent_id: "NY",
+      x: "815",
+      y: "158"
+    },
+    PA: {
+      parent_id: "PA",
+      x: "786",
+      y: "210"
+    },
+    VA: {
+      parent_id: "VA",
+      x: "790",
+      y: "282"
+    },
+    WV: {
+      parent_id: "WV",
+      x: "744",
+      y: "270"
+    },
+    OH: {
+      parent_id: "OH",
+      x: "700",
+      y: "240"
+    },
+    IN: {
+      parent_id: "IN",
+      x: "650",
+      y: "250"
+    },
+    IL: {
+      parent_id: "IL",
+      x: "600",
+      y: "250"
+    },
+    WI: {
+      parent_id: "WI",
+      x: "575",
+      y: "155"
+    },
+    NC: {
+      parent_id: "NC",
+      x: "784",
+      y: "326"
+    },
+    TN: {
+      parent_id: "TN",
+      x: "655",
+      y: "340"
+    },
+    AR: {
+      parent_id: "AR",
+      x: "548",
+      y: "368"
+    },
+    MO: {
+      parent_id: "MO",
+      x: "548",
+      y: "293"
+    },
+    GA: {
+      parent_id: "GA",
+      x: "718",
+      y: "405"
+    },
+    SC: {
+      parent_id: "SC",
+      x: "760",
+      y: "371"
+    },
+    KY: {
+      parent_id: "KY",
+      x: "680",
+      y: "300"
+    },
+    AL: {
+      parent_id: "AL",
+      x: "655",
+      y: "405"
+    },
+    LA: {
+      parent_id: "LA",
+      x: "550",
+      y: "435"
+    },
+    MS: {
+      parent_id: "MS",
+      x: "600",
+      y: "405"
+    },
+    IA: {
+      parent_id: "IA",
+      x: "525",
+      y: "210"
+    },
+    MN: {
+      parent_id: "MN",
+      x: "506",
+      y: "124"
+    },
+    OK: {
+      parent_id: "OK",
+      x: "460",
+      y: "360"
+    },
+    TX: {
+      parent_id: "TX",
+      x: "425",
+      y: "435"
+    },
+    NM: {
+      parent_id: "NM",
+      x: "305",
+      y: "365"
+    },
+    KS: {
+      parent_id: "KS",
+      x: "445",
+      y: "290"
+    },
+    NE: {
+      parent_id: "NE",
+      x: "420",
+      y: "225"
+    },
+    SD: {
+      parent_id: "SD",
+      x: "413",
+      y: "160"
+    },
+    ND: {
+      parent_id: "ND",
+      x: "416",
+      y: "96"
+    },
+    WY: {
+      parent_id: "WY",
+      x: "300",
+      y: "180"
+    },
+    MT: {
+      parent_id: "MT",
+      x: "280",
+      y: "95"
+    },
+    CO: {
+      parent_id: "CO",
+      x: "320",
+      y: "275"
+    },
+    UT: {
+      parent_id: "UT",
+      x: "223",
+      y: "260"
+    },
+    AZ: {
+      parent_id: "AZ",
+      x: "205",
+      y: "360"
+    },
+    NV: {
+      parent_id: "NV",
+      x: "140",
+      y: "235"
+    },
+    OR: {
+      parent_id: "OR",
+      x: "100",
+      y: "120"
+    },
+    WA: {
+      parent_id: "WA",
+      x: "130",
+      y: "55"
+    },
+    ID: {
+      parent_id: "ID",
+      x: "200",
+      y: "150"
+    },
+    CA: {
+      parent_id: "CA",
+      x: "79",
+      y: "285"
+    },
+    MI: {
+      parent_id: "MI",
+      x: "663",
+      y: "185"
+    },
+    PR: {
+      parent_id: "PR",
+      x: "620",
+      y: "545"
+    },
+    GU: {
+      parent_id: "GU",
+      x: "550",
+      y: "540"
+    },
+    VI: {
+      parent_id: "VI",
+      x: "680",
+      y: "519"
+    },
+    MP: {
+      parent_id: "MP",
+      x: "570",
+      y: "575"
+    },
+    AS: {
+      parent_id: "AS",
+      x: "665",
+      y: "580"
+    }
+  },
+  regions: {}
+};
